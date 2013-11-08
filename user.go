7892e6a0e71b0c8afaa6d316830bc3471b78899f
V. Glenn Tarcea <gtarcea@umich.edu>
2013-11-08 15:38:33 -0500

    Fillout loading the users projects.

--- user.go
+++ user.go
@@ -1,34 +1,57 @@
 package materials
 
 import (
+	"bufio"
+	"os"
 	"os/user"
+	"path/filepath"
+	"strings"
 )
 
 type Project struct {
-	Name string
-	dir string
+	Name      string `json:"name" xml:"name"`
+	Directory string `json:"directory" xml:"directory"`
 }
 
 type User struct {
 	user     *user.User
-	projects map[string]string
+	Projects []Project
 }
 
-func New() (User, error) {
+func CurrentUser() (*User, error) {
 	u, err := user.Current()
 
 	if err != nil {
-		return User{}, err
+		return nil, err
 	}
 
 	usr := User{
-		user:     u,
-		projects: make(map[string]string),
+		user: u,
 	}
 
 	usr.loadProjects()
+
+	return &usr, nil
 }
 
 func (u *User) loadProjects() {
+	projectsFile, err := os.Open(u.projectsFile())
 
+	if err != nil {
+		return
+	}
+
+	defer projectsFile.Close()
+
+	scanner := bufio.NewScanner(projectsFile)
+	for scanner.Scan() {
+		splitLine := strings.Split(scanner.Text(), "|")
+		if len(splitLine) == 2 {
+			u.Projects = append(u.Projects, Project{splitLine[0], splitLine[1]})
+		}
+	}
+}
+
+func (u *User) projectsFile() string {
+	return filepath.Join(u.user.HomeDir, ".materials", "projects")
 }
