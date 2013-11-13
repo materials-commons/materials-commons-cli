db35b9bf5f84d3052d459df7605b2ad6e89a0e2a
V. Glenn Tarcea <gtarcea@umich.edu>
2013-11-13 16:27:28 -0500

    Start filling out an upload file interface.

--- upload.go
+++ upload.go
@@ -1,6 +1,9 @@
 package materials
 
 import (
+	"bytes"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -14,5 +17,28 @@ func (p Project) UploadProject() error {
 }
 
 func newFileUploadRequest(uri string, params map[string]string, paramName, path string) (*http.Request, error) {
-	return nil, nil
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	part, _ := writer.CreateFormFile(paramName, filepath.Base(path))
+
+	_, err = io.Copy(part, file)
+	writer.WriteField("fullpath", path)
+
+	for key, val := range params {
+		writer.WriteField(key, val)
+	}
+
+	writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return http.NewRequest("POST", uri, body)
 }
