b8c3cc66c3bf4f44ff4144d731ba09f6cd0c5b31
V. Glenn Tarcea <gtarcea@umich.edu>
2013-11-13 15:19:41 -0500

    Add in unit tests for Find

--- projects_test.go
+++ projects_test.go
@@ -9,29 +9,29 @@ func TestNonExistantUser(t *testing.T) {
 	username := "no-such-user-xxx"
 	_, err := ProjectsForUser(username)
 	if err == nil {
-		t.Errorf("Should not have found user '%s'\n", username)
+		t.Fatalf("Should not have found user '%s'\n", username)
 	}
 }
 
 func TestProjectsFrom(t *testing.T) {
 	projects, err := ProjectsFrom("test_data")
 	if err != nil {
-		t.Errorf("TsetProjectsFrom failed loading the test_data projects")
+		t.Fatalf("TsetProjectsFrom failed loading the test_data projects\n")
 	}
 
 	if len(projects.Projects()) != 2 {
-		t.Errorf("Number of projects incorrect, it should have been 2: %d", len(projects.Projects()))
+		t.Fatalf("Number of projects incorrect, it should have been 2: %d\n", len(projects.Projects()))
 	}
 }
 
 func TestProjectsFromWithBadDirectory(t *testing.T) {
 	projects, err := ProjectsFrom("no-such-directory")
 	if err == nil {
-		t.Errorf("ProjectFrom should have returned an error")
+		t.Fatalf("ProjectFrom should have returned an error\n")
 	}
 
 	if projects != nil {
-		t.Errorf("projects should have been nil")
+		t.Fatalf("projects should have been nil\n")
 	}
 }
 
@@ -40,11 +40,11 @@ func TestReadCorruptedProjectsFile(t *testing.T) {
 	length := len(projects.Projects())
 
 	if err != nil {
-		logFail(t, "err should have been nil")
+		t.Fatalf("err should have been nil\n")
 	}
 
 	if length != 2 {
-		t.Errorf("Expected corrupted projects to be 2, got %d", length)
+		t.Fatalf("Expected corrupted projects to be 2, got %d\n", length)
 	}
 }
 
@@ -52,7 +52,7 @@ func TestProjectAddDuplicate(t *testing.T) {
 	p, _ := ProjectsFrom("test_data")
 	err := p.Add(Project{Name: "proj1", Path: "/tmp"})
 	if err == nil {
-		t.Fatalf("Duplicate project was added")
+		t.Fatalf("Duplicate project was added\n")
 	}
 
 	p2, _ := ProjectsFrom("test_data")
@@ -69,18 +69,18 @@ func TestProjectAdd(t *testing.T) {
 	p, _ := ProjectsFrom("test_data")
 	err := p.Add(Project{Name: "new proj", Path: "/tmp"})
 	if err != nil {
-		logFail(t, "Add failed to add new project")
+		t.Fatalf("Add failed to add new project\n")
 	}
 
 	l := len(p.Projects())
 	if l != 3 {
-		logFail(t, "Expected number of projects to be 3, got %d", l)
+		t.Fatalf("Expected number of projects to be 3, got %d\n", l)
 	}
 
 	p2, _ := ProjectsFrom("test_data")
 	l = len(p2.Projects())
 	if l != 3 {
-		logFail(t, "Expected number of projects to be 3, got %d", l)
+		t.Fatalf("Expected number of projects to be 3, got %d\n", l)
 	}
 }
 
@@ -88,13 +88,13 @@ func TestProjectRemove(t *testing.T) {
 	p, _ := ProjectsFrom("test_data")
 	err := p.Remove("new proj")
 	if err != nil {
-		logFail(t, "Remove failed to add new project")
+		t.Fatalf("Remove failed to add new project\n")
 	}
 
 	p2, _ := ProjectsFrom("test_data")
 	l := len(p2.Projects())
 	if l != 2 {
-		logFail(t, "Expected number of projects to be 2, got %d", l)
+		t.Fatalf("Expected number of projects to be 2, got %d\n", l)
 	}
 }
 
@@ -105,12 +105,32 @@ func TestProjectExists(t *testing.T) {
 	}
 
 	if !p.Exists("proj1") {
-		t.Fatalf("Failed to find project that should exist: proj1")
+		t.Fatalf("Failed to find project that should exist: proj1\n")
 	}
 }
 
-func logFail(t *testing.T, formatString string, args ...interface{}) {
-	formatString += "\n"
-	t.Errorf(formatString, args)
-	t.FailNow()
+func TestProjectFind(t *testing.T) {
+	p, _ := ProjectsFrom("test_data")
+
+	_, found := p.Find("does-not-exist")
+	if found {
+		t.Fatalf("Found project that does not exist")
+	}
+
+	_, found = p.Find("proj1")
+	if !found {
+		t.Fatalf("Did not find project proj1\n")
+	}
+
+	p.Add(Project{Name: "newproj", Path: "/tmp/newproj"})
+	_, found = p.Find("newproj")
+	if !found {
+		t.Fatalf("Did not find added project newproj\n")
+	}
+
+	p.Remove("newproj")
+	_, found = p.Find("newproj")
+	if found {
+		t.Fatalf("Found project that was just removed: newproj\n")
+	}
 }
