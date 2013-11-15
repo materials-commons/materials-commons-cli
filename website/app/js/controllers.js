157f5524db00970c99cb91e5e1f65e0be31fb245
V. Glenn Tarcea <gtarcea@umich.edu>
2013-11-15 11:26:47 -0500

    Remove testing code for going to materials commons.

--- website/app/js/controllers.js
+++ website/app/js/controllers.js
@@ -3,21 +3,15 @@ function HomeController($scope) {
 
 }
 
-function ProjectsController($scope, Restangular) {
+function ProjectsController($scope, Restangular, $http) {
     var allProjects = Restangular.all('projects');
     allProjects.getList().then(function(projects) {
         $scope.projects = projects;
     });
 
     Restangular.one("projects", "a").customGET("tree").then(function(tree) {
-        console.dir(tree);
         $scope.projectTree = tree;
     });
-
-//    Restangular.oneUrl("datadirs", "http://localhost:5000/datadirs/tree?apikey=4a3ec8f43cc511e3ba368851fb4688d4")
-//        .get().then(function(tree) {
-//            console.dir(tree);
-//        })
 }
 
 function ChangesController($scope) {
