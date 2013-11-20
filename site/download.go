efcc97007625603338d14f736c7d1cca5cf95f9b
V. Glenn Tarcea <gtarcea@umich.edu>
2013-11-20 14:41:26 -0500

    Remove some code refactoring that was incorrect.

--- site/download.go
+++ site/download.go
@@ -44,36 +44,3 @@ func getDownloadedVersionOfWebsite() int {
 	//content := ioutil.ReadFile()
 	return 0
 }
-
-func unpackWebsite(path string) {
-	file, _ := os.Open(path)
-	defer file.Close()
-
-	zhandle, _ := gzip.NewReader(file)
-	defer zhandle.Close()
-
-	thandle := tar.NewReader(zhandle)
-	for {
-		hdr, err := thandle.Next()
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			break
-		}
-
-		if hdr.Typeflag == tar.TypeDir {
-			dirpath := filepath.Join(mcuser.DotMaterialsPath(), hdr.Name)
-			os.MkdirAll(dirpath, 0777)
-		} else if hdr.Typeflag == tar.TypeReg || hdr.Typeflag == tar.TypeRegA {
-			filepath := filepath.Join(mcuser.DotMaterialsPath(), hdr.Name)
-			out, _ := os.Create(filepath)
-			if _, err := io.Copy(out, thandle); err != nil {
-				fmt.Println(err)
-			}
-			out.Close()
-		}
-	}
-	8
-}
