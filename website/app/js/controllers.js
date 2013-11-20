8ff246c53cabf5ea9010c7ff0bb64683c4042d50
Glenn Tarcea <glenn.tarcea@gmail.com>
2013-11-19 22:28:54 -0500

    Updates to webpage to:   - Show project tree when project is selected   - Add projects from the webpage   - Move functionality for project status template to file   - Add different state buttons for project status

--- website/app/js/controllers.js
+++ website/app/js/controllers.js
@@ -25,47 +25,66 @@ function ProjectsController($scope, Restangular, $http) {
         console.dir(val);
     };
 
+    $scope.newProject = function() {
+        console.log("Creating project: " + $scope.newProjectName);
+        console.log("  Located at: " + $scope.newProjectPath);
+        var proj = {
+            name: $scope.newProjectName,
+            path: $scope.newProjectPath,
+            status: "Unloaded"
+        }
+        allProjects.post(proj).then(function() {
+            console.log("Project created");
+            allProjects.getList().then(function (projects) {
+                $scope.projects = projects;
+            });
+        }, function() {
+            console.log("Project creation failed!");
+        });
+        $scope.newProjectName = "";
+        $scope.newProjectPath = "";
+    }
+
     $scope.projectGridOptions = {
         data: 'projects',
         multiSelect: false,
         columnDefs: [
             {field: 'name', displayName: 'Name'},
             {field: 'path', displayName: 'Path'},
-            {field: 'status', displayName: 'Status',
-                cellTemplate: '<div>{{ row.entity[col.field] }}  <button ng-click="statusButtonAction(row.entity)">{{ statusButtonName(row.entity[col.field]) }}</button></div>'}
+            {field: 'status', displayName: 'Status', cellTemplate: 'partials/projects/status_cell.html'}
         ],
         selectedItems: $scope.selected,
-        afterSelectionChange: function () {
-            //console.dir($scope.selected);
+        afterSelectionChange: function (rowItem) {
+            $scope.project = rowItem.entity.name;
+            $scope.projectStatus = rowItem.entity.status;
+            Restangular.one("projects", $scope.project).customGET("tree").then(function (tree) {
+                $scope.projectTree = tree;
+            });
         }
     };
 
-    Restangular.one("projects", "a").customGET("tree").then(function (tree) {
-        $scope.projectTree = tree;
-    });
+
 }
 
 function ChangesController($scope, Restangular, $timeout) {
     $scope.events = [];
-    (function tick() {
-        console.log("tick")
-        Restangular.all('projects/changes').getList().then(function(eventsInfo) {
-            //console.dir(eventsInfo);
-            var found = false;
-            $scope.events.forEach(function(event) {
-                if (event.filepath == eventsInfo.filepath) {
-                    found = true;
-                }
-            });
-
-            if (! found) {
-                $scope.events.push(eventsInfo);
-            }
-        })
-        $timeout(tick, 3000);
-    })();
-
-
+//    (function tick() {
+//        console.log("tick")
+//        Restangular.all('projects/changes').getList().then(function(eventsInfo) {
+//            //console.dir(eventsInfo);
+//            var found = false;
+//            $scope.events.forEach(function(event) {
+//                if (event.filepath == eventsInfo.filepath) {
+//                    found = true;
+//                }
+//            });
+//
+//            if (! found) {
+//                $scope.events.push(eventsInfo);
+//            }
+//        })
+//        $timeout(tick, 3000);
+//    })();
 }
 
 function ProvenanceController($scope) {
