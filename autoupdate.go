378471613745500c2916acd3f37a58fb93bef266
Glenn Tarcea <glenn.tarcea@gmail.com>
2013-11-25 21:54:09 -0500

    Layout getting and testing the remote binary to see if it needs to be updated.

--- autoupdate.go
+++ autoupdate.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	//"github.com/materials-commons/gohandy/ezhttp"
+	"hash/crc32"
+	"io/ioutil"
 )
 
 func Restart() {
@@ -17,4 +20,24 @@ func Restart() {
 	command := exec.Command("nohup", commandPath, "--server", "--retry=10")
 	command.Start()
 	os.Exit(0)
+}
+
+func Download(url string) {
+	path, _ := osext.Executable()
+	myChecksum := checksumFor(path)
+	path = downloadNewBinary(url)
+	fmt.Printf("%d", myChecksum)
+}
+
+func checksumFor(path string) uint32 {
+	file, _ := os.Open(path)
+	defer file.Close()
+	c := crc32.NewIEEE()
+	bytes, _ := ioutil.ReadAll(file)
+	withcrc := c.Sum(bytes)
+	return crc32.ChecksumIEEE(withcrc)
+}
+
+func downloadNewBinary(url string) string {
+	return ""
 }
