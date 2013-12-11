3f19af05e67957b97764249efa3ee4832d41f5b6
V. Glenn Tarcea <gtarcea@umich.edu>
2013-12-11 10:49:30 -0500

    Add directive for treetable.

---
+++ website2/app/scripts/directives/treetable.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('materialsApp')
+    .directive('treetable', function ($timeout) {
+        return {
+            restrict: 'A',
+            scope: {
+                treedata: '='
+            },
+            link: function (scope, element) {
+                scope.$watch('treedata', function (newValue, oldValue) {
+                    if (newValue) {
+                        $timeout(function () {
+                            $(element).treetable({expandable: true}, true);
+                        }, 0);
+                    }
+                });
+            }
+        };
+    });
