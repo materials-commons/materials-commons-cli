82a3b07d05aa22b5d9ea7acada142148f2fa13e1
V. Glenn Tarcea <gtarcea@umich.edu>
2013-12-12 13:50:55 -0500

    Fix to make compile and to monitor multiple projects each with their own recursive watcher.

--- wsmaterials/monitor.go
+++ wsmaterials/monitor.go
@@ -29,34 +29,51 @@ func startHttp(retryCount int, sio *socketio.SocketIOServer) {
 func monitorProjectChanges(sio *socketio.SocketIOServer) {
 	p, _ := materials.CurrentUserProjects()
 
-	var projectPaths []string
 	for _, project := range p.Projects() {
-		projectPaths = append(projectPaths, project.Path)
+		startProjectWatcher(project, sio)
 	}
 
-	watcher, err := materials.NewRecursiveWatcherPaths([]string{"/tmp/a", "/tmp/b"})
-	if err != nil {
-		return
-	}
-	watcher.Run()
-	defer watcher.Close()
-
-	for {
-		select {
-		case file := <-watcher.Files:
-			//fmt.Printf("File changed: %s\n", file)
-			pfs := ProjectFileStatus{
-				FilePath: file,
-				Status:   "File Changed",
+	/*
+		for {
+			select {
+			case file := <-watcher.Files:
+				//fmt.Printf("File changed: %s\n", file)
+				pfs := ProjectFileStatus{
+					FilePath: file,
+					Status:   "File Changed",
+				}
+				sio.Broadcast("file", &pfs)
+			case folder := <-watcher.Folders:
+				//fmt.Printf("Folder changed: %s\n", folder)
+				pfs := ProjectFileStatus{
+					FilePath: folder,
+					Status:   "Directory Changed",
+				}
+				sio.Broadcast("dir", &pfs)
 			}
-			sio.Broadcast("file", &pfs)
-		case folder := <-watcher.Folders:
-			//fmt.Printf("Folder changed: %s\n", folder)
-			pfs := ProjectFileStatus{
-				FilePath: folder,
-				Status:   "Directory Changed",
-			}
-			sio.Broadcast("dir", &pfs)
 		}
-	}
+	*/
+}
+
+func startProjectWatcher(project materials.Project, sio *socketio.SocketIOServer) {
+	go func() {
+		watcher, err := materials.NewRecursiveWatcher(project.Path)
+		if err != nil {
+			return
+		}
+		watcher.Run()
+		defer watcher.Close()
+
+		for {
+			select {
+			case e := <-watcher.Events:
+				pfs := ProjectFileStatus{
+					FilePath: e.Name,
+					Status:   "Something",
+				}
+
+				sio.Broadcast("file", &pfs)
+			}
+		}
+	}()
 }
