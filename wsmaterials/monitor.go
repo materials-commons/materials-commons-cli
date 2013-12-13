b9cb5ca1f2b0e8d32874f9c542a9d8bba7ff791a
Glenn Tarcea <glenn.tarcea@gmail.com>
2013-12-13 13:42:35 -0500

    Significant code cleanup and commenting. Updated when watcher was moved to gohandy.

--- wsmaterials/monitor.go
+++ wsmaterials/monitor.go
@@ -1,29 +1,41 @@
 package wsmaterials
 
+/*
+ * monitor.go contains the routines for monitoring project files and directory changes. It
+ * communicates with the frontend using socket.io. Each project is monitored separately.
+ */
+
 import (
 	"fmt"
 	"github.com/googollee/go-socket.io"
+	"github.com/materials-commons/gohandy/fs"
 	"github.com/materials-commons/materials"
 	"net/http"
 	"os"
 	"time"
 )
 
-type ProjectFileStatus struct {
+// projectFileStatus communicates the types of changes that
+// have occured.
+type projectFileStatus struct {
 	Project  string `json:"project"`
 	FilePath string `json:"filepath"`
-	Status   string `json:"status"`
+	Event    string `json:"event"`
 }
 
+// startMonitor starts the monitor service and the HTTP and SocketIO connections.
 func startMonitor() {
 	sio := socketio.NewSocketIOServer(&socketio.Config{})
 	sio.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		// Nothing to do right now
+		// Nothing to do
 	})
 	go monitorProjectChanges(sio)
 	go startHttp(10, sio)
 }
 
+// startHttp starts up a HTTP server. It will attempt to start the server
+// retryCount times. The retry on server startup handles the case where
+// the old materials service is stopping, and the new one is starting.
 func startHttp(retryCount int, sio *socketio.SocketIOServer) {
 	for i := 0; i < retryCount; i++ {
 		fmt.Println(http.ListenAndServe(":8082", sio))
@@ -32,39 +44,41 @@ func startHttp(retryCount int, sio *socketio.SocketIOServer) {
 	os.Exit(1)
 }
 
+// monitorProjectChanges starts a separate go thread for each project to monitor.
 func monitorProjectChanges(sio *socketio.SocketIOServer) {
 	p, _ := materials.CurrentUserProjects()
 
 	for _, project := range p.Projects() {
-		startProjectWatcher(project, sio)
+		go projectWatcher(project, sio)
 	}
 }
 
-func startProjectWatcher(project materials.Project, sio *socketio.SocketIOServer) {
-	go func() {
-		watcher, err := materials.NewRecursiveWatcher(project.Path)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		watcher.Run()
-		defer watcher.Close()
+// projectWatcher starts the file system monitor. It watches for file system
+// events and then communicates them along the SocketIOServer. It sends events
+// to the front end as projectFileStatus messages encoded in JSON.
+func projectWatcher(project materials.Project, sio *socketio.SocketIOServer) {
+	watcher, err := fs.NewRecursiveWatcher(project.Path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	watcher.Run()
+	defer watcher.Close()
 
-		for {
-			select {
-			case e := <-watcher.Events:
-				pfs := &ProjectFileStatus{
-					Project:  project.Name,
-					FilePath: e.Name,
-					Status:   eventStatus(e),
-				}
-				sio.Broadcast("file", pfs)
-			}
+	for {
+		event := <-watcher.Events
+		pfs := &projectFileStatus{
+			Project:  project.Name,
+			FilePath: event.Name,
+			Event:    eventType(event),
 		}
-	}()
+		sio.Broadcast("file", pfs)
+	}
 }
 
-func eventStatus(event materials.Event) string {
+// eventType takes an event determines what type of event occurred and
+// returns the corresponding string.
+func eventType(event fs.Event) string {
 	switch {
 	case event.IsCreate():
 		return "Created"
