dbbde79752f7473ceeb56b2a62adaa999c7e0da7
V. Glenn Tarcea <gtarcea@umich.edu>
2013-12-19 17:06:10 -0500

    Extend the ProjectFileInfo type to track file sync and location status.

--- ftracker.go
+++ ftracker.go
@@ -10,12 +10,62 @@ import (
 	"time"
 )
 
+type ProjectFileStatus int
+
+const (
+	Synchronized ProjectFileStatus = iota
+	Unsynchronized
+	New
+	Deleted
+)
+
+func (pfs ProjectFileStatus) String() string {
+	switch {
+	case pfs&Synchronized == Synchronized:
+		return "Synchronized"
+	case pfs&Unsynchronized == Unsynchronized:
+		return "Unsynchronized"
+	case pfs&New == New:
+		return "New"
+	case pfs&Deleted == Deleted:
+		return "Deleted"
+	default:
+		return "Unknown"
+	}
+}
+
+type ProjectFileLocation int
+
+const (
+	LocalOnly ProjectFileLocation = iota
+	RemoteOnly
+	LocalAndRemote
+	LocalAndRemoteUnknown
+)
+
+func (pfl ProjectFileLocation) String() string {
+	switch {
+	case pfl&LocalOnly == LocalOnly:
+		return "LocalOnly"
+	case pfl&RemoteOnly == RemoteOnly:
+		return "RemoteOnly"
+	case pfl&LocalAndRemote == LocalAndRemote:
+		return "LocalAndRemote"
+	case pfl&LocalAndRemoteUnknown == LocalAndRemoteUnknown:
+		return "LocalAndRemoteUnknown"
+	default:
+		return "Unknown"
+	}
+}
+
 type ProjectFileInfo struct {
 	Path    string
 	Size    int64
 	Hash    string
 	ModTime time.Time
 	Id      string
+	Status  ProjectFileStatus
+	Location ProjectFileLocation
 }
 
 func (project *Project) Walk() error {
