0de09c63e9debed6504d5e54ad27e0b4527a05e1
V. Glenn Tarcea <gtarcea@umich.edu>
2013-12-19 14:32:05 -0500

    Fix issue with computing hash. We need to create a new instance of MD5 for hash we generate.

--- ftracker.go
+++ ftracker.go
@@ -3,6 +3,7 @@ package materials
 import (
 	"crypto/md5"
 	"encoding/json"
+	"fmt"
 	"github.com/materials-commons/gohandy/handyfile"
 	"os"
 	"path/filepath"
@@ -10,24 +11,23 @@ import (
 )
 
 type ProjectFileInfo struct {
-	Path     string
-	Size     int64
-	Checksum []byte
-	ModTime  time.Time
-	Id       string
+	Path    string
+	Size    int64
+	Hash    string
+	ModTime time.Time
+	Id      string
 }
 
 func (project *Project) Walk() error {
-	hasher := md5.New()
 	filepath.Walk(project.Path, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
-			key := hasher.Sum([]byte(path))
-			checksum, _ := handyfile.Hash(hasher, path)
+			key := md5.New().Sum([]byte(path))
+			checksum, _ := handyfile.Hash(md5.New(), path)
 			pinfo := &ProjectFileInfo{
-				Path:     path,
-				Size:     info.Size(),
-				Checksum: checksum,
-				ModTime:  info.ModTime(),
+				Path:    path,
+				Size:    info.Size(),
+				Hash:    fmt.Sprintf("%x", checksum),
+				ModTime: info.ModTime(),
 			}
 			value, _ := json.Marshal(pinfo)
 			project.Put(key, value, nil)
