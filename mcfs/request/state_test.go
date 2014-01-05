fb45637403efc71412d2bf83b944d3c356dfb418
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-01-04 21:35:44 -0500

    Remove server.

--- mcfs/request/state_test.go
+++ mcfs/request/state_test.go
@@ -1,13 +1,45 @@
 package request
 
 import (
-	//	"encoding/gob"
-	//	"net"
+	"encoding/gob"
+	"fmt"
 	r "github.com/dancannon/gorethink"
+	"github.com/materials-commons/materials/transfer"
 	"io"
-	//	"github.com/materials-commons/materials/transfer"
+	"net"
+	"os"
 	"testing"
 )
+
+var session *r.Session
+
+func init() {
+	session, _ = r.Connect(map[string]interface{}{
+		"address":  "localhost:30815",
+		"database": "materialscommons",
+	})
+
+	//gob.Register(transfer.LoginReq{})
+}
+
+type client struct {
+	*gob.Encoder
+	*gob.Decoder
+}
+
+func newClient() *client {
+	conn, err := net.Dial("tcp", "localhost:35862")
+	if err != nil {
+		fmt.Printf("Couldn't connect %s\n", err.Error())
+		os.Exit(1)
+	}
+	encoder := gob.NewEncoder(conn)
+	decoder := gob.NewDecoder(conn)
+	return &client{
+		Encoder: encoder,
+		Decoder: decoder,
+	}
+}
 
 func tdatafileOpen(dfid string) (io.WriteCloser, error) {
 	return nil, nil
@@ -22,7 +54,28 @@ func tdatafileWrite(w io.WriteCloser, bytes []byte) (int, error) {
 }
 
 func TestLoginLogout(t *testing.T) {
+	client := newClient()
+	fmt.Println("got client")
 
+	loginReq := transfer.LoginReq{
+		ProjectID: "abc123",
+		User:      "gtarcea@umich.edu",
+		ApiKey:    "472abe203cd411e3a280ac162d80f1bf",
+	}
+	req := transfer.Request{
+		Type: transfer.Login,
+		Req:  loginReq,
+	}
+
+	fmt.Println("sending req")
+	err := client.Encode(&req)
+	fmt.Println(err)
+	fmt.Println("req sent")
+	resp := transfer.Response{}
+	fmt.Println("getting resp")
+	err = client.Decode(&resp)
+	fmt.Println(err)
+	fmt.Printf("%#v", resp)
 }
 
 func TestCreate(t *testing.T) {
