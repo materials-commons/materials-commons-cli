9ae5f5c34c9a134776653e622e84a1feeb12d2aa
V. Glenn Tarcea <gtarcea@umich.edu>
2014-01-13 17:29:23 -0500

    Pass in the mcdir directory.

--- mcfs/request/datafile.go
+++ mcfs/request/datafile.go
@@ -5,11 +5,11 @@ import (
 	"strings"
 )
 
-func datafileDir(dataFileID string) string {
+func datafileDir(mcdir, dataFileID string) string {
 	pieces := strings.Split(dataFileID, "-")
-	return filepath.Join("/mcfs/data/materialscommons", pieces[1][0:2], pieces[1][2:4])
+	return filepath.Join(mcdir, pieces[1][0:2], pieces[1][2:4])
 }
 
-func datafilePath(dataFileID string) string {
-	return filepath.Join(datafileDir(dataFileID), dataFileID)
+func datafilePath(mcdir, dataFileID string) string {
+	return filepath.Join(datafileDir(mcdir, dataFileID), dataFileID)
 }
