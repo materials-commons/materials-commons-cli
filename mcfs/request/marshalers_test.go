b538fd08386cf75e617e5f645c4ae09afdf1381d
V. Glenn Tarcea <gtarcea@umich.edu>
2014-01-15 17:28:27 -0500

    Not really tests, just a way to see what a marshaler is doing.

--- mcfs/request/marshalers_test.go
+++ mcfs/request/marshalers_test.go
@@ -22,3 +22,34 @@ func TestRequestMarshaler(t *testing.T) {
 		t.Fatalf("Inner item not being properly saved")
 	}
 }
+
+func TestChannelMarshaler(t *testing.T) {
+	m := util.NewChannelMarshaler()
+	go responder(m)
+	loginReq := transfer.LoginReq{
+		User:   "gtarcea@umich.edu",
+		ApiKey: "abc123",
+	}
+	req := transfer.Request{
+		Req: loginReq,
+	}
+
+	m.Marshal(&req)
+	var resp transfer.Response
+	m.Unmarshal(&resp)
+	fmt.Printf("resp = %#v\n", resp)
+	l := resp.Resp.(*transfer.LogoutResp)
+	fmt.Printf("l = %#v\n, *l = %#v\n", l, *l)
+}
+
+func responder(m *util.ChannelMarshaler) {
+	var request transfer.Request
+	m.Unmarshal(&request)
+	fmt.Printf("request = %#v\n", request)
+	logoutResp := transfer.LogoutResp{}
+	resp := transfer.Response{
+		Type: transfer.ROk,
+		Resp: &logoutResp,
+	}
+	m.Marshal(&resp)
+}
