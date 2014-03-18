d6a9705495ff291bc4e6ade4c735fd9a69fb63d0
V. Glenn Tarcea <gtarcea@umich.edu>
2014-03-18 14:30:37 -0400

    Updates for golint.

--- autoupdate/updater.go
+++ autoupdate/updater.go
@@ -5,12 +5,15 @@ import (
 	"github.com/materials-commons/materials/site"
 )
 
+// A Updater keeps track of the status of binary and website updates
+// and downloads updates when they are avaiable.
 type Updater struct {
 	downloaded     string
 	websiteUpdated bool
 	binaryUpdated  bool
 }
 
+// NewUpdater creates a new Updater instance.
 func NewUpdater() *Updater {
 	return &Updater{
 		downloaded:     "",
@@ -19,6 +22,8 @@ func NewUpdater() *Updater {
 	}
 }
 
+// UpdatesAvailable checks if updates are available for either the website
+// or the materials binary. If updates are available it will download them.
 func (u *Updater) UpdatesAvailable() bool {
 	updateAvailable := false
 	if downloaded, err := site.Download(); err == nil {
@@ -37,6 +42,8 @@ func (u *Updater) UpdatesAvailable() bool {
 	return updateAvailable
 }
 
+// ApplyUpdates deploys updates that have been downloaded. If the materials
+// binary has been updated then it restarts the server.
 func (u *Updater) ApplyUpdates() {
 	if u.websiteUpdated {
 		site.Deploy(u.downloaded)
@@ -49,10 +56,12 @@ func (u *Updater) ApplyUpdates() {
 	}
 }
 
+// WebsiteUpdate returns true if the website has been updated.
 func (u *Updater) WebsiteUpdate() bool {
 	return u.websiteUpdated
 }
 
+// BinaryUpdate returns true if the materials binary has been updated.
 func (u *Updater) BinaryUpdate() bool {
 	return u.binaryUpdated
 }
