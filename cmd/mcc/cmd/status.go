06f4771527cea081d05a613c0aef6cf0b4660199
gtarcea <gtarcea@umich.edu>
2023-05-08 17:05:37 -0400

    Move project walking code out of status and into mcc so it can be used in other commands. Update walker to call callback methods when encountering an unknown or changed file. Update status to call the project walker.

--- cmd/mcc/cmd/status.go
+++ cmd/mcc/cmd/status.go
@@ -7,17 +7,12 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 	"sync"
 
 	"github.com/materials-commons/materials-commons-cli/pkg/config"
+	"github.com/materials-commons/materials-commons-cli/pkg/mcc"
 	"github.com/materials-commons/materials-commons-cli/pkg/mcdb"
-	"github.com/materials-commons/materials-commons-cli/pkg/model"
-	"github.com/materials-commons/materials-commons-cli/pkg/stor"
-	"github.com/materials-commons/materials-commons-cli/pkg/util"
-	"github.com/saracen/walker"
 	"github.com/spf13/cobra"
-	"gorm.io/gorm"
 )
 
 // statusCmd represents the status command
@@ -27,8 +22,9 @@ var statusCmd = &cobra.Command{
 	Long:  `Gets the status of local files and identifies what should be uploaded.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		db := mcdb.MustConnectToDB()
-		sw := newStatusWalker(db)
-		if err := sw.Walk(config.GetProjectRootPath()); err != nil {
+		sw := &statusWalkerState{}
+		projectWalker := mcc.NewProjectWalker(db, sw.changedFileHandler, sw.unknownFileHandler)
+		if err := projectWalker.Walk(config.GetProjectRootPath()); err != nil {
 			log.Fatalf("Unable to get status: %s", err)
 		}
 
@@ -36,74 +32,22 @@ var statusCmd = &cobra.Command{
 	},
 }
 
-type statusWalker struct {
-	ignoredFileStor stor.IgnoredFileStor
-	fileStor        stor.FileStor
-	unknownFiles    sync.Map
-	changedFiles    sync.Map
+type statusWalkerState struct {
+	unknownFiles sync.Map
+	changedFiles sync.Map
 }
 
-func newStatusWalker(db *gorm.DB) *statusWalker {
-	return &statusWalker{
-		ignoredFileStor: stor.NewGormIgnoredFileStor(db),
-		fileStor:        stor.NewGormFileStor(db),
-	}
-}
-
-func (w *statusWalker) Walk(path string) error {
-	return walker.Walk(path, w.walkCallback, walker.WithErrorCallback(w.walkerErrorCallback))
-}
-
-func (w *statusWalker) walkCallback(path string, finfo os.FileInfo) error {
-	if finfo.IsDir() && path == config.GetProjectMCDirPath() {
-		return filepath.SkipDir
-	}
-
-	filename := filepath.Base(path)
-	if filename == ".mcignore" {
-		return nil
-	}
-
-	projectPath := util.ToProjectPath(path)
-
-	if w.ignoredFileStor.FileIsIgnored(projectPath) {
-		return nil
-	}
-
-	f, err := w.fileStor.GetFileByPath(projectPath)
-	if err != nil {
-		// Error looking up file, assume it's an unknown file
-		w.unknownFiles.Store(projectPath, path)
-		return nil
-	}
-
-	if finfo.IsDir() {
-		// If we are here then this is a known directory. There is nothing
-		// we need to do for the directory.
-		return nil
-	}
-
-	if !fileIsChanged(f, finfo) {
-		return nil
-	}
-
-	// If we are here then the file is known and the mtime indicates its probably
-	// changed so add it to the changed file list
+func (w *statusWalkerState) changedFileHandler(projectPath, path string, _ os.FileInfo) error {
 	w.changedFiles.Store(projectPath, path)
-
 	return nil
 }
 
-func (w *statusWalker) walkerErrorCallback(path string, err error) error {
-	if os.IsPermission(err) {
-		return nil
-	}
-
-	// Halt on any other error
-	return err
+func (w *statusWalkerState) unknownFileHandler(projectPath, path string, _ os.FileInfo) error {
+	w.unknownFiles.Store(projectPath, path)
+	return nil
 }
 
-func (w *statusWalker) ShowStatus() {
+func (w *statusWalkerState) ShowStatus() {
 	fmt.Println("Unknown:")
 	w.unknownFiles.Range(func(key any, value any) bool {
 		path := value.(string)
@@ -117,18 +61,6 @@ func (w *statusWalker) ShowStatus() {
 		fmt.Println(path)
 		return true
 	})
-}
-
-func fileIsChanged(f *model.File, finfo os.FileInfo) bool {
-	if f.LMtime.Before(finfo.ModTime()) {
-		// If the Local MTime we have for this file is before the MTime in the file system
-		// then the file has potentially changed. We will only know for sure by computing
-		// the checksum, but for now we can just set this file as changed and a candidate
-		// to upload.
-		return true
-	}
-
-	return false
 }
 
 func init() {
---
+++ pkg/mcc/walker.go
@@ -0,0 +1,97 @@
+package mcc
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/materials-commons/materials-commons-cli/pkg/config"
+	"github.com/materials-commons/materials-commons-cli/pkg/model"
+	"github.com/materials-commons/materials-commons-cli/pkg/stor"
+	"github.com/materials-commons/materials-commons-cli/pkg/util"
+	"github.com/saracen/walker"
+	"gorm.io/gorm"
+)
+
+type ProjectWalkerHandlerFn func(projectPath, realPath string, finfo os.FileInfo) error
+
+type ProjectWalker struct {
+	ignoredFileStor      stor.IgnoredFileStor
+	fileStor             stor.FileStor
+	ChangedFileHandlerFn ProjectWalkerHandlerFn
+	UnknownFileHandlerFn ProjectWalkerHandlerFn
+}
+
+func NewProjectWalker(db *gorm.DB, changedFileHandlerFn, unknownFileHandlerFn ProjectWalkerHandlerFn) *ProjectWalker {
+	return &ProjectWalker{
+		ignoredFileStor:      stor.NewGormIgnoredFileStor(db),
+		fileStor:             stor.NewGormFileStor(db),
+		ChangedFileHandlerFn: changedFileHandlerFn,
+		UnknownFileHandlerFn: unknownFileHandlerFn,
+	}
+}
+
+func (w *ProjectWalker) Walk(path string) error {
+	return walker.Walk(path, w.walkCallback, walker.WithErrorCallback(w.walkerErrorCallback))
+}
+
+func (w *ProjectWalker) walkCallback(path string, finfo os.FileInfo) error {
+	if finfo.IsDir() && path == config.GetProjectMCDirPath() {
+		return filepath.SkipDir
+	}
+
+	filename := filepath.Base(path)
+	if filename == ".mcignore" {
+		return nil
+	}
+
+	projectPath := util.ToProjectPath(path)
+
+	if w.ignoredFileStor.FileIsIgnored(projectPath) {
+		return nil
+	}
+
+	f, err := w.fileStor.GetFileByPath(projectPath)
+	if err != nil {
+		// Error looking up file, assume it's an unknown file
+		if err := w.UnknownFileHandlerFn(projectPath, path, finfo); err != nil {
+			// do something
+		}
+		return nil
+	}
+
+	if finfo.IsDir() {
+		// If we are here then this is a known directory. There is nothing
+		// we need to do for the directory.
+		return nil
+	}
+
+	if w.fileIsChanged(f, finfo) {
+		if err := w.ChangedFileHandlerFn(projectPath, path, finfo); err != nil {
+			// do something
+		}
+		return nil
+	}
+
+	return nil
+}
+
+func (w *ProjectWalker) walkerErrorCallback(path string, err error) error {
+	if os.IsPermission(err) {
+		return nil
+	}
+
+	// Halt on any other error
+	return err
+}
+
+func (w *ProjectWalker) fileIsChanged(f *model.File, finfo os.FileInfo) bool {
+	if f.LMtime.Before(finfo.ModTime()) {
+		// If the Local MTime we have for this file is before the MTime in the file system
+		// then the file has potentially changed. We will only know for sure by computing
+		// the checksum, but for now we can just set this file as changed and a candidate
+		// to upload.
+		return true
+	}
+
+	return false
+}
