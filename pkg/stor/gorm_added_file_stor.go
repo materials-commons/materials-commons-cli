41577072058efd63e965a0975ae2057452a4be33
gtarcea <gtarcea@umich.edu>
2023-05-15 13:30:01 -0400

    Add comments and implement RemoveAll and RemoveByPath

--- pkg/stor/gorm_added_file_stor.go
+++ pkg/stor/gorm_added_file_stor.go
@@ -14,6 +14,8 @@ func NewGormAddedFileStor(db *gorm.DB) *GormAddedFileStor {
 	return &GormAddedFileStor{db: db}
 }
 
+// AddFile will add a new AddedFile to the added_files table. Note that the path column is unique
+// and the add will fail if there is already an entry matching that path.
 func (s *GormAddedFileStor) AddFile(path, reason, ftype string) (*model.AddedFile, error) {
 	fileToAdd := &model.AddedFile{
 		Path:   path,
@@ -28,6 +30,7 @@ func (s *GormAddedFileStor) AddFile(path, reason, ftype string) (*model.AddedFil
 	return fileToAdd, err
 }
 
+// GetFileByPath returns the AddFile matching path.
 func (s *GormAddedFileStor) GetFileByPath(path string) (*model.AddedFile, error) {
 	var f model.AddedFile
 
@@ -40,4 +43,14 @@ func (s *GormAddedFileStor) GetFileByPath(path string) (*model.AddedFile, error)
 // stop execution.
 func (s *GormAddedFileStor) ListPaged(fn func(f *model.AddedFile) error) error {
 	return listPaged(s.db, fn)
+}
+
+// RemoveAll will remove all added files in the add_files tables.
+func (s *GormAddedFileStor) RemoveAll() error {
+	return s.db.Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&model.AddedFile{}).Error
+}
+
+// RemoveByPath will remove an entry in the add_files table that matches path.
+func (s *GormAddedFileStor) RemoveByPath(path string) error {
+	return s.db.Where("path = ?", path).Delete(&model.AddedFile{}).Error
 }
